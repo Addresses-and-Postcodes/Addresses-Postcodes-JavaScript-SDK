9cfa8a4bb1e95507befec7704d3221a657186b3a
Moussa Ball <moiseball20155@gmail.com>
2022-08-19 09:44:05 +0000

    Install & Setup Eslint For Typescript.

--- src/Client.ts
+++ src/Client.ts
@@ -1,36 +1,36 @@
-const axios = require('axios').default;
+import axios from 'axios';
 
 /**
  * Client Class
  */
 export class Client {
-    /**
-     * The api key.
-     */
-    private _api_key: string
+  /**
+      * The api key.
+      */
+  private _api_key: string;
 
-    /**
-     * Host
-     */
-    private _host: string = "https://postcodes.test/"
+  /**
+      * Host
+      */
+  private _host = 'https://postcodes.test/';
 
-    /**
-     * constructor.
-     */
-    constructor(apikey: string) {
-        this._api_key = apikey
-    }
+  /**
+      * constructor.
+      */
+  constructor(apikey: string) {
+    this._api_key = apikey;
+  }
 
-    /**
-     * HTTP Request|GET
-     * 
-     * @param uri api endpoint
-     */
-    public async get(uri: string): Promise<any> {
-        let url = new URL(uri, this._host)
-        let endpoint = `${url.toString()}?key=${this._api_key}`
+  /**
+      * HTTP Request|GET
+      * 
+      * @param uri api endpoint
+      */
+  public async get(uri: string): Promise<any> {
+    const url = new URL(uri, this._host);
+    const endpoint = `${url.toString()}?key=${this._api_key}`;
 
-        // HTTP REQUEST|GET
-        axios.get(endpoint).then(response => { return response }).catch(error => { return error })
-    }
+    // HTTP REQUEST|GET
+    axios.get(endpoint).then(response => { return response; }).catch(error => { return error; });
+  }
 }
